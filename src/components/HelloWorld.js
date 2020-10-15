025905932f5511a50e1030a2bb5fece0d122d53f
Kris Torres <krist@skechers.com>
2020-10-15 14:30:45 -0700

    Move the footer to the HelloWorld component

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,19 +1,23 @@
 import React from "react";
 
 import FlexBox from "./FlexBox.js";
+import Footer from "./Footer.js";
 import SignIn from "./views/SignIn.js";
 
 function HelloWorld() {
     return (
-        <FlexBox
-            component="main"
-            level={1}
-            justifyContent="flex-start"
-            alignItems="center"
-            className="mdc-theme--background hello-world"
-        >
-            <SignIn/>
-        </FlexBox>
+        <React.Fragment>
+            <FlexBox
+                component="main"
+                level={1}
+                justifyContent="flex-start"
+                alignItems="center"
+                className="mdc-theme--background hello-world"
+            >
+                <SignIn/>
+            </FlexBox>
+            <Footer/>
+        </React.Fragment>
     );
 }
 
